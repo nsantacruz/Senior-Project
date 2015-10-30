3aa47f57cf9c3a7204f48f87bd01d6b86baa081f
nsantacruz <noahssantacruz@gmail.com>
2015-10-29 23:00:57 -0400

    added extract ENF test 3 which extends howie's work and adds in filtering. hope to add qifft soon...

---
+++ filterENF.m
@@ -0,0 +1,38 @@
+function [filteredSignal] = filterENF(signal,f,df,decFactor)
+% takes in signal, decimates by decFator and returns bandpass
+% f is mid freq, df is deviation frequency s.t. filteredSignal is in f +/-
+% df
+    %A = 60, C = 60
+    bOrd = 3;       
+    fs = 1000;
+    wlen = 8192;        
+    n = round(log2(wlen));
+    nfft = 2^n;
+    fn = fs/2;
+    f1 = (f-df)*decFactor; f2 = (f+df)*decFactor;
+    w1 = f1/fn; w2 = f2/fn;
+    Wp = [w1 w2];
+
+    %ad = decimate(a,3);
+    signal = decimate(signal,decFactor);
+
+    [b,a] = butter(bOrd,Wp);
+    filteredSignal = filter(b,a,signal);
+    %PLOTTING
+    if false
+        figure;
+        spectrogram(signal,wlen,4096,nfft,fs,'yaxis');
+        title('Spectrogram of Orig Signal');
+        figure;
+        spectrogram(filteredSignal,wlen,4096,nfft,fs,'yaxis');
+        title('Spectrogram of Filtered Signal');
+        figure;
+        startind = 2e5;
+        plot(filteredSignal(startind:startind+1e4));
+        title('Sample of time domain filtered signal');
+        figure;
+        plot(signal(startind:startind+1e4));
+        title('Sample of time domain original signal');
+    end
+end
+
