4df9eb975daf85c4a4a5e8caafa21b0809a5be47
JonathanWeinrib <jonathanweinrib@gmail.com>
2015-11-23 23:13:48 -0500

    put in classifiers stuff

---
+++ classtest.py
@@ -0,0 +1,233 @@
+
+
+import numpy as np 
+
+
+
+#from sklearn import svm
+from sklearn.svm import SVC
+from sklearn.linear_model import LogisticRegression as logreg
+from sklearn.feature_selection import SelectFromModel as sfm
+from sklearn import neighbors
+from sklearn.neighbors.nearest_centroid import NearestCentroid
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as linda
+from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis as quadda
+from sklearn.tree import DecisionTreeClassifier as DTC
+from sklearn.tree import ExtraTreeClassifier as ETC
+from sklearn.ensemble import BaggingClassifier
+from sklearn.ensemble import RandomForestClassifier as RFC
+from sklearn.ensemble import AdaBoostClassifier as ABC
+from sklearn.ensemble import GradientBoostingClassifier as GBC
+from sklearn.ensemble import VotingClassifier
+
+#def myclassify(numfiers = 6,xtrain=xtrain,ytrain=ytrain,xtest=xtest,ytest=ytest):
+def myclassify(numfiers,xtrain,ytrain,xtest,ytest):
+    count = 0
+    print numfiers
+
+
+    bagging2 = BaggingClassifier(ETC(),bootstrap=False,bootstrap_features=False)
+    bagging2.fit(xtrain,ytrain)
+    #print bagging2.score(xtest,ytest)
+    count += 1
+    classifiers = [bagging2.score(xtest,ytest)]
+    print "trained 1 classifier \n"
+
+    if count < numfiers:
+
+        tree2 = ETC()
+        tree2.fit(xtrain,ytrain)
+        #print tree2.fit(xtrain,ytrain)
+        #print tree2.score(xtest,ytest)
+        count+=1
+        classifiers = np.append(classifiers,tree2.score(xtest,ytest))
+        print "trained 2 classifiers \n"
+
+    if count < numfiers:
+        bagging1 = BaggingClassifier(ETC())
+        bagging1.fit(xtrain,ytrain)
+        #print bagging1.score(xtest,ytest)
+        count+=1
+        classifiers = np.append(classifiers,bagging1.score(xtest,ytest))
+        print "trained 3 classifiers \n"
+
+    if count < numfiers:
+        # votingClassifiers combine completely different machine learning classifiers and use a majority vote
+        clff1 = SVC()
+        clff2 = RFC(bootstrap=False)
+        clff3 = ETC()
+        clff4 = neighbors.KNeighborsClassifier()
+        clff5 = quadda()
+
+
+
+        eclf = VotingClassifier(estimators = [('svc',clff1),('rfc',clff2),('etc',clff3),('knn',clff4),('qda',clff5)])
+        eclf = eclf.fit(xtrain,ytrain)
+        #print(eclf.score(xtest,ytest))
+        # for claf, label in zip([clff1,clff2,clff3,clff4,clff5,eclf],['SVC','RFC','ETC','KNN','QDA','Ensemble']):
+        #     cla
+        #     scores = crossvalidation.cross_val_score(claf,xtrain,ytrain,scoring='accuracy')
+        #     print ()
+        count+=1
+        classifiers = np.append(classifiers,eclf.score(xtest,ytest))
+        print "trained 4 classifiers \n"
+
+
+    if count < numfiers:
+        svc1 = SVC()
+        svc1.fit(xtrain,ytrain)
+        dec = svc1.score(xtest,ytest)
+        count+=1
+        classifiers = np.append(classifiers,svc1.score(xtest,ytest))
+        print "trained 5 classifiers \n"
+
+    if count < numfiers:
+        # Quadradic discriminant analysis - classifier with quadratic decision boundary -
+        qda = quadda()
+        qda.fit(xtrain,ytrain)
+        #print(qda.score(xtest,ytest))
+        count+=1
+        classifiers = np.append(classifiers,qda.score(xtest,ytest))
+        print "trained 6 classifiers \n"
+
+
+
+    if count < numfiers:
+
+        tree1 = DTC()
+        tree1.fit(xtrain,ytrain)
+        #print tree1.fit(xtrain,ytrain)
+        #print tree1.score(xtest,ytest)
+        count+=1
+        classifiers = np.append(classifiers,tree1.score(xtest,ytest))
+        print "trained 7 classifiers \n"
+
+    if count < numfiers:
+        knn1 = neighbors.KNeighborsClassifier() # this classifies based on the #k nearest neighbors, where k is definted by the user.
+        knn1.fit(xtrain,ytrain)
+        #print(knn1.score(xtest,ytest))
+        count+=1
+        classifiers = np.append(classifiers,knn1.score(xtest,ytest))
+
+
+    if count < numfiers:
+        # linear discriminant analysis - classifier with linear decision boundary -
+        lda = linda()
+        lda.fit(xtrain,ytrain)
+        #print(lda.score(xtest,ytest))
+        count+=1
+        classifiers = np.append(classifiers,lda.score(xtest,ytest))
+
+    if count < numfiers:
+        tree3 = RFC()
+        tree3.fit(xtrain,ytrain)
+        #print tree3.score(xtest,ytest)
+        count+=1
+        classifiers = np.append(classifiers,tree3.score(xtest,ytest))
+
+    if count < numfiers:
+        bagging3 = BaggingClassifier(RFC(),bootstrap=False,bootstrap_features=False)
+        bagging3.fit(xtrain,ytrain)
+        #print bagging3.score(xtest,ytest)
+        count+=1
+        classifiers = np.append(classifiers,bagging3.score(xtest,ytest))
+
+
+    if count < numfiers:
+        bagging4 = BaggingClassifier(SVC(),bootstrap=False,bootstrap_features=False)
+        bagging4.fit(xtrain,ytrain)
+        #print bagging4.score(xtest,ytest)
+        count+=1
+        classifiers = np.append(classifiers,bagging4.score(xtest,ytest))
+
+    if count < numfiers:
+        tree4 = RFC(bootstrap=False)
+        tree4.fit(xtrain,ytrain)
+        #print tree4.score(xtest,ytest)
+        count+=1
+        classifiers = np.append(classifiers,tree4.score(xtest,ytest))
+
+    if count < numfiers:
+        tree6 = GBC()
+        tree6.fit(xtrain,ytrain)
+        #print(tree6.score(xtest,ytest))
+        count+=1
+        classifiers = np.append(classifiers,tree6.score(xtest,ytest))
+
+    if count < numfiers:
+        knn2 = neighbors.KNeighborsClassifier(n_neighbors = 10)
+        knn2.fit(xtrain,ytrain)
+        #print(knn2.score(xtest,ytest))
+        count+=1
+        classifiers = np.append(classifiers,knn2.score(xtest,ytest))
+
+    if count < numfiers:
+        knn3 = neighbors.KNeighborsClassifier(n_neighbors = 3)
+        knn3.fit(xtrain,ytrain)
+        #print(knn3.score(xtest,ytest))
+        count+=1
+        classifiers = np.append(classifiers,knn3.score(xtest,ytest))
+
+    if count < numfiers:
+        knn4 = neighbors.KNeighborsClassifier(algorithm = 'ball_tree')
+        knn4.fit(xtrain,ytrain)
+        #print(knn4.score(xtest,ytest))
+        count+=1
+        classifiers = np.append(classifiers,knn4.score(xtest,ytest))
+
+    if count < numfiers:
+        knn5 = neighbors.KNeighborsClassifier(algorithm = 'kd_tree')
+        knn5.fit(xtrain,ytrain)
+        #print(knn5.score(xtest,ytest))
+        count+=1
+        classifiers = np.append(classifiers,knn5.score(xtest,ytest))
+
+    if count < numfiers:
+        ncc1 = NearestCentroid()
+        ncc1.fit(xtrain,ytrain)
+        #print (ncc1.score(xtest,ytest))
+        count+=1
+        classifiers = np.append(classifiers,ncc1.score(xtest,ytest))
+
+    if count < numfiers:
+    # Nearest shrunken Centroid
+        for shrinkage in [None,0.05,0.1,0.2,0.3,0.4,0.5]:
+            ncc2 = NearestCentroid(shrink_threshold = shrinkage)
+            ncc2.fit(xtrain,ytrain)
+            #print(ncc2.score(xtest,ytest))
+
+        count+=1
+        classifiers = np.append(classifiers,ncc2.score(xtest,ytest))
+
+    if count < numfiers:
+        tree5 = ABC()
+        tree5.fit(xtrain,ytrain)
+        #print(tree5.score(xtest,ytest))
+        count+=1
+        classifiers = np.append(classifiers,tree5.score(xtest,ytest))
+
+    classifierlabel = ["BaggingETC (with bootstraps set to false)","ETC","BaggingETC","Voting Classifier","svm","QDA","DTC","KNN (default)","LDA","RFC",
+                       "BaggingRFC (with bootstraps set to false)","BaggingSVC (with bootstraps set to false)","RFC (bootstrap false)","GBC",
+                        "knn (n_neighbors = 10)","knn (n_neighbors = 3)","knn (ball tree algorithm)","knn (kd_tree algorithm)",
+                       "Nearest Centroid","Shrunken Centroid?","ABC"]
+
+
+    classifierlabel = classifierlabel[:len(classifiers)]
+
+    for i in range(len(classifiers)):
+
+
+        print ("{} classifier has percent correct {}".format(classifierlabel[i],classifiers[i]))
+
+
+
+
+
+
+
+
+#
+# print "Hi"
+#
+# myclassify(6,xtrain,ytrain1,xtest,ytest1)
+# myclassify(12,xtrain,ytrain1,xtest,ytest1)
---
+++ main.py
@@ -0,0 +1,110 @@
+
+
+
+import classtest
+import scipy.io as sio
+import numpy as np
+
+xtrain=sio.loadmat('xtrain.mat')
+# print xtrain
+# print "done!"
+xtrain=xtrain['xtrain']
+ytrain=sio.loadmat('ytrain.mat')
+ytrain = ytrain['ytrain']
+xtest=sio.loadmat('xtest.mat')
+xtest = xtest['xtest']
+ytest=sio.loadmat('ytest.mat')
+ytest=ytest['ytest']
+
+ytrain1 = np.ravel(ytrain)
+ytest1 = np.ravel(ytest)
+
+print xtrain.shape
+print xtest.shape
+
+
+classtest.myclassify(numfiers=20,xtrain=xtrain,ytrain=ytrain1,xtest=xtest,ytest=ytest1)
+
+# xtrainwo = sio.loadmat('xtrainwo.mat')
+# # print xtrainwo
+# xtrainwo = xtrainwo['xtrain']
+# ytrainwo = sio.loadmat('ytrainwo.mat')
+# ytrainwo = ytrainwo['ytrain']
+# xtestwo = sio.loadmat('xtestwo.mat')
+# xtestwo = xtestwo['xtest']
+# ytestwo = sio.loadmat('ytestwo.mat')
+# ytestwo = ytestwo['ytest']
+#
+# ytrain1new = np.ravel(ytrainwo)
+# ytest1new = np.ravel(ytestwo)
+#
+# # classtest.myclassify(numfiers=6,xtrain=xtrainwo,ytrain=ytrain1new,xtest=xtestwo,ytest=ytest1new)
+#
+# print xtrainwo.shape
+# print xtestwo.shape
+#
+#
+
+
+
+
+
+
+
+# from sklearn import datasets
+# import imp_fil_mat
+# import scipy.io as sio
+# #sio.matlab
+# from sklearn import svm
+# from sklearn.svm import LinearSVC
+# from sklearn.svm import SVC
+#
+# import numpy as np
+# from sklearn import linear_model
+#
+# #import test
+# #test.myfunc("hello")
+#
+#
+#
+# #imp_fil_mat.train_test()
+# #xtrain,xtest,ytrain,ytest = imp_fil_mat.train_test()
+#
+#
+# # MAKE THIS A FUNCTION!! having trouble
+# import scipy.io as sio
+# sio.matlab
+# featmat = sio.loadmat('NUMPYTESTFEATURES.mat')
+# #ww = sio.whosmat('NUMPYTESTFEATURES.mat')
+# #print ww
+# #print featmat
+# xtrain = featmat['xtrain']
+# xtest = featmat['xtest']
+# ytrain = featmat['ytrain']
+# ytest = featmat['ytest']
+#
+# # print xtrain.shape
+# # print ytrain.shape
+# # print xtest.shape
+# # print ytest.shape
+#
+# ytrain1 = np.ravel(ytrain)
+# ytest1 = np.ravel(ytest)
+#
+# clf = SVC()
+# clf.fit(xtrain,ytrain1)
+# dec = clf.score(xtest,ytest1)
+# #results = clf.score(xtest,ytest)
+# print dec
+#
+# # lin_clf = LinearSVC()
+# # lin_clf.fit(xtrain,ytrain1)
+# # dec1 = lin_clf.score(xtest,ytest1)
+# #print dec1
+# # this took a ridiculous amount of time, and only got 49%
+#
+#
+# lassoclf = linear_model.Lasso(alpha = 0.1)
+# lassoclf.fit(xtrain,ytrain)
+# dec2 = lassoclf.score(xtest,ytest)
+# print dec2
