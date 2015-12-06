bac37ca4148d250b98c8bff9c2aab629145819a9
JonathanWeinrib <jonathanweinrib@gmail.com>
2015-12-06 13:33:48 -0500

    stuff

--- mainMATLAB.m
+++ mainMATLAB.m
@@ -3,26 +3,40 @@
 
 clc; clear all; close all;
 
-numTestRecs = 0;
+numTestRecs = 2;
 isP = 1;
 Norm = 0;
 Est = 0;
-name1 = 'ybintrain_all_pow.mat';
-name2 = 'ybintest_all_pow.mat';
-name3 = 'xtrain_all_pow.mat';
-name4 = 'ytrain_all_pow.mat';
+name1 = 'ybintrain_A_pow.mat';
+name2 = 'ybintest_A_pow.mat';
+name3 = 'xtrain_Apow.mat';
+name4 = 'ytrain_Apow.mat';
+name5 = 'xtest_Apow.mat';
+name6 = 'ytest_Apow.mat';
 [xtrain_all_pow, ytrain_all_pow, xtest, ytest, ybintrain_all_pow, ybintest_all_pow]...
     = FeatExtract(numTestRecs,...
-    isP, Norm, Est, name1, name2, name3, name4);
+    isP, Norm, Est, name1, name2, name3, name4,name5,name6);
 
-numTestRecs = 0;
-isP = 0;
-Norm = 0;
-Est = 0;
-name1 = 'ybintrain_all_aud.mat';
-name2 = 'ybintest_all_aud.mat';
-name3 = 'xtrain_all_aud.mat';
-name4 = 'ytrain_all_aud.mat';
-[xtrain_all_aud, ytrain_all_aud, xtest, ytest, ybintrain_all_aud, ybintest_all_aud]...
-    = FeatExtract(numTestRecs,...
-    isP, Norm, Est, name1, name2, name3, name4);
+% numTestRecs = 0;
+% isP = 0;
+% Norm = 0;
+% Est = 0;
+% name1 = 'ybintrain_aud.mat';
+% name2 = 'ybintest_aud.mat';
+% name3 = 'xtrain_aud.mat';
+% name4 = 'ytrain_aud.mat';% numTestRecs = 0;
+% isP = 0;
+% Norm = 0;
+% Est = 0;
+% name1 = 'ybintrain_aud.mat';
+% name2 = 'ybintest_aud.mat';
+% name3 = 'xtrain_aud.mat';
+% name4 = 'ytrain_aud.mat';
+% name5 = 'ytrain_aud.mat';
+% [xtrain_aud, ytrain_aud, xtest_aud, ytest_aud, ybintrain_aud, ybintest_aud]...
+%     = FeatExtract(numTestRecs,...
+%     isP, Norm, Est, name1, name2, name3, name4);
+% name5 = 'ytrain_aud.mat';
+% [xtrain_aud, ytrain_aud, xtest_aud, ytest_aud, ybintrain_aud, ybintest_aud]...
+%     = FeatExtract(numTestRecs,...
+%     isP, Norm, Est, name1, name2, name3, name4);
